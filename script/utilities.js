157652e3e44573b167e265a651b51b29c8d5e06f
sagorsenanupam <senanupam10@gmail.com>
2025-06-22 23:18:42 +0600

    edited

--- script/utilities.js
+++ script/utilities.js
@@ -4,8 +4,21 @@ function assignedTask(id) {
 
 function acitivtyLog(id) {
   const text = document.getElementById("activity-log").innerText;
+
   const title = id.parentNode.parentNode.querySelector(".card-title").innerText;
+  // -----------------------
+  // took help from google
+
+  const now = new Date();
+  const time = now.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+  const date = now.toLocaleDateString();
+  // -------------
+
   document.getElementById("activity-log").innerHTML += `
-  <p class='mb-5 rounded-lg p-2 bg-[#F4F7FF] justify-center items-center text-center'>You have completed the task ${title} at that time</p>
+  <p class='mb-1 rounded-lg p-2 bg-[#F4F7FF] justify-center items-center text-center text-sm'>You have completed the task ${title} at <span style="color:green;">${time}</span> on ${date}</p>
 `;
 }
