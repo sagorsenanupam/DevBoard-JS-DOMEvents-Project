8be5256cb355340a404e99b7fc108a66eeb3ba64
sagorsenanupam <senanupam10@gmail.com>
2025-06-22 22:27:18 +0600

    edited

--- script/date-time.js
+++ script/date-time.js
@@ -1,21 +1,36 @@
 function updateDate() {
-    const now = new Date();
+  const now = new Date();
 
-    const dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-    const day = dayNames[now.getDay()];
+  const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const day = dayNames[now.getDay()];
 
-    const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    const month = monthNames[now.getMonth()];
-    
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const month = monthNames[now.getMonth()];
 
-    const date = now.getDate();
-    const year = now.getFullYear();
-    
-    const formattedDate = `${day},\n<span class='font-bold'>${month} ${date} ${year}</span>`;
-    
-    document.getElementById('datetime').innerHTML = formattedDate.replace('\n', '<br>');
-    
-    setTimeout(updateDate, 60000);
+  const date = now.getDate();
+  const year = now.getFullYear();
+
+  const formattedDate = `${day},\n<span class='font-bold'>${month} ${date} ${year}</span>`;
+
+  document.getElementById("datetime").innerHTML = formattedDate.replace(
+    "\n",
+    "<br>"
+  );
+
+  setTimeout(updateDate, 60000);
 }
 
 window.onload = updateDate();
--- script/utilities.js
+++ script/utilities.js
@@ -6,6 +6,6 @@ function acitivtyLog(id) {
   const text = document.getElementById("activity-log").innerText;
   const title = id.parentNode.parentNode.querySelector(".card-title").innerText;
   document.getElementById("activity-log").innerHTML += `
-  <p class='mb-5 p-2 bg-[#F4F7FF]'>You have completed the task ${title} at that time</p>
+  <p class='mb-5 rounded-lg p-2 bg-[#F4F7FF] justify-center items-center text-center'>You have completed the task ${title} at that time</p>
 `;
 }
