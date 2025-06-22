4a957782e2be75c5ff3559f0ce98062e8067da44
sagorsenanupam <senanupam10@gmail.com>
2025-06-22 23:59:50 +0600

    d

--- script/theme-btn.js
+++ script/theme-btn.js
@@ -1,3 +1,4 @@
 document.getElementById("theme-btn").addEventListener("click", function () {
-  console.log("clicked");
+  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = randomColor;
 });
